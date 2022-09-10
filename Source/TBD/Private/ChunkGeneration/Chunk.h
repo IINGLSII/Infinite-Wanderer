71b7a082a733a861c5bd8f1587a4ddca49d7eb78
IINGLSII <nickolassimons367@gmail.com>
2022-09-10 03:08:44 -0500

    started chunk generation

---
+++ Source/TBD/Private/ChunkGeneration/Chunk.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Chunk.generated.h"
+
+UCLASS()
+class AChunk : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AChunk();
+
+	static const float size;
+
+	// returns int corresponding to player position in 3x3, 0-8 grid.
+	UFUNCTION()
+	uint8 check_player_adjacency(FVector location) const;
+
+protected:
+	// Called when the game starts or when spawned
+
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
